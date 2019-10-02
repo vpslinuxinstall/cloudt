9f99a959d68ddf4e9c94339a2b859931089650b4
vpslinuxinstall <38807387+vpslinuxinstall@users.noreply.github.com>
2019-10-01 20:56:01 -0400

    Create cloudt.sh

---
+++ cloudt.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+wget -P /usr/local/bin/ https://github.com/vpslinuxinstall/cloudt/releases/download/1/cloud-torrent
+chmod +x /usr/local/bin/cloud-torrent
+/usr/local/bin/cloud-torrent --port 8080 --auth user:password
+
+
+
+
